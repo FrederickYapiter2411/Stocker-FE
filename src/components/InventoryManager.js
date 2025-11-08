469b64b341968591a7e8170908b488bff5f25ecb
FrederickYapiter2411 <165285425+FrederickYapiter2411@users.noreply.github.com>
2025-11-08 22:37:16 +0700

    Update InventoryManager.js
    Fix: Change API_URL

--- src/components/InventoryManager.js
+++ src/components/InventoryManager.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './InventoryManager.css';
 
-const API_URL = `http://stock_be:5000/api/items`; // 'http://host.docker.internal:5000/api/items';
+const API_URL = `/api/items`; // 'http://host.docker.internal:5000/api/items';
 
 const InventoryManager = () => {
     const [items, setItems] = useState([]);
