981d057350146ef4a1e463443a35ea17f509da39
FrederickYapiter2411 <165285425+FrederickYapiter2411@users.noreply.github.com>
2025-11-09 21:23:37 +0700

    fis url

--- src/components/InventoryManager.js
+++ src/components/InventoryManager.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './InventoryManager.css';
 
-const API_URL = process.env.REACT_APP_BACKEND;
+const API_URL = 'http://stock_be:5000/api/items';
 
 const InventoryManager = () => {
     const [items, setItems] = useState([]);
